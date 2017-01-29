17a0065513dfb34ce81c3fbd1982f25924d9b7fa
sarathi0333 <sarath.kct@gmail.com>
2017-01-29 10:46:46 +0530

    git commit

--- systemjs.config.js
+++ systemjs.config.js
@@ -4,24 +4,20 @@
  */
 
 System.config({
-paths: {
-    // paths serve as alias
-    'npm:': 'node_modules/'
-},
 // map tells the System loader where to look for things
 map: {
     // our app is within the app folder
     app: 'app',
 
     // angular bundles
-    '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-    '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-    '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-    '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/core': 'node_modules/@angular/core/bundles/core.umd.js',
+    '@angular/common': 'node_modules/@angular/common/bundles/common.umd.js',
+    '@angular/compiler': 'node_modules/@angular/compiler/bundles/compiler.umd.js',
+    '@angular/platform-browser': 'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic': 'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
 
     // other libraries
-    'rxjs':                      'npm:rxjs'
+    'rxjs':                      'node_modules/rxjs'
 },
 // packages tells the System loader how to load when no filename and/or no extension
 packages: {
